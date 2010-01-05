61584399e6dcd6a965897c69fdfee3a683f6a2be
Dieter Plaetinck <dieter@plaetinck.be>
2010-01-05 21:17:35 +0100

    merge from master

--- uzbl.c
+++ uzbl.c
@@ -923,7 +923,6 @@ eval_js(WebKitWebView * web_view, gchar *script, GString *result) {
     WebKitWebFrame *frame;
     JSGlobalContextRef context;
     JSObjectRef globalobject;
-    JSStringRef var_name;
 
     JSStringRef js_script;
     JSValueRef js_result;
@@ -935,12 +934,6 @@ eval_js(WebKitWebView * web_view, gchar *script, GString *result) {
     frame = webkit_web_view_get_main_frame(WEBKIT_WEB_VIEW(web_view));
     context = webkit_web_frame_get_global_context(frame);
     globalobject = JSContextGetGlobalObject(context);
-    
-    /* uzbl javascript namespace */
-    var_name = JSStringCreateWithUTF8CString("Uzbl");
-    JSObjectSetProperty(context, globalobject, var_name,
-                        JSObjectMake(context, uzbl.js.classref, NULL),  
-                        kJSClassAttributeNone, NULL);
     
     /* evaluate the script and get return value*/ 
     js_script = JSStringCreateWithUTF8CString(script);
@@ -959,9 +952,6 @@ eval_js(WebKitWebView * web_view, gchar *script, GString *result) {
     }
 
     /* cleanup */
-    JSObjectDeleteProperty(context, globalobject, var_name, NULL);
-
-    JSStringRelease(var_name);
     JSStringRelease(js_script);
 }
 
